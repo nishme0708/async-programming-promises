c7744cd421850c430ef78a0f9f4542eac2af9f9b
Nishant Mehta <42231185+nishme0708@users.noreply.github.com>
2022-02-06 13:50:27 +0530

    Add consuming code

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,16 +1,59 @@
-import setText, {appendText, showWaiting, hideWaiting} from "./results.mjs";
+import setText, { appendText, showWaiting, hideWaiting } from './results.mjs';
 
 export function get() {
+    axios.get('http://localhost:3000/orders/1').then(({ data }) => {
+        setText(JSON.stringify(data));
+    });
 }
 
 export function getCatch() {
+    axios
+        .get('http://localhost:3000/orders/123')
+        .then((results) => {
+            if (results.status == '200') {
+                setText(JSON.stringify(results.data));
+            } else {
+                setText('error');
+            }
+        })
+        .catch((e) => setText(e));
 }
 
 export function chain() {
+    axios
+        .get('http://localhost:3000/orders/1')
+        .then(({ data }) => {
+            return axios.get('http://localhost:3000/addresses/' + data.shippingAddress);
+        })
+        .then(({ data }) => {
+            setText(JSON.stringify(data));
+        });
 }
 
 export function chainCatch() {
+    axios
+        .get('http://localhost:3000/orders/1')
+        .then(({ data }) => {
+             return axios.get('http://localhost:3000/addresses/' + data.shippingAddress);
+             throw new Error('test');
+        })
+        .catch(err=>setText(err))
+        .then(({ data }) => {
+            setText(JSON.stringify(data.my.city));
+
+        }).catch(err=>setText(err));
 }
 
 export function final() {
+    showWaiting();
+    axios
+        .get('http://localhost:3000/orders/1')
+        .then(({ data }) => {
+            return axios.get('http://localhost:3000/addresses/' + data.shippingAddress);
+        })
+        .then(({ data }) => {
+            setText(JSON.stringify(data));
+        }).finally(()=>{
+            hideWaiting();
+        });
 }
